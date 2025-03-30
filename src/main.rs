59b6fdaafcc6824122193c2a6002a0e25bb902de
Dieguillo <dieper76@gmail.com>
2025-03-30 18:04:48 -0300

    Optimización del código

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,29 @@
 // Recibe los argumentos por línea de comandos
 
 use std::env;
+use std::process;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    if args.len() > 1 {
-        // println!("Argumentos:");
-        // for arg in &args[1..] {
-        //     println!(" - {}", arg); }
-
-        let args: Vec<String> = env::args().collect();
-    
-        // Combinar todos los argumentos en un String
-        let texto: String = args[1..].join(" ");
-    
-        let texto_lcd: String = texto //move?
-            .chars()
-            .map(|c| match c {
-                '0' => '🯰', '1' => '🯱',
-                '2' => '🯲', '3' => '🯳',
-                '4' => '🯴', '5' => '🯵',
-                '6' => '🯶', '7' => '🯷',
-                '8' => '🯸', '9' => '🯹',
-                _ => c,
-            })
-            .collect();
-            println!("{texto_lcd}");
-    } else {
-        println!("Uso: {} <alfanum> <alfanum> ..", &args[0]);
+    if args.len() <= 1 {
+        eprintln!("Uso: {} <alfanum> <alfanum> ..", &args[0]);
+        process::exit(1);
     }
+
+    // Combinar todos los argumentos en un String
+    let texto: String = args[1..].join(" ")
+        .chars()
+        .map(|c| match c {
+            '0' => '🯰', '1' => '🯱',
+            '2' => '🯲', '3' => '🯳',
+            '4' => '🯴', '5' => '🯵',
+            '6' => '🯶', '7' => '🯷',
+            '8' => '🯸', '9' => '🯹',
+            _ => c,
+        })
+        .collect();
+
+    println!("{texto}");
 }
